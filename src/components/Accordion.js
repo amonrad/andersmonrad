a27635f7e4893e6f52710f9baa80da71f1ca2c01
amonrad <amonrad@hotmail.com>
2025-03-05 12:06:01 +0100

    updated Accordion
    Accordion contains TailwindCSS instead of plain CSSCSS

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -25,7 +25,10 @@ export default function Accordion({ title, imageSrc, content, styles, isOpen, on
                 </div>
             </div>
 
+
             {/* Open State Content (Always Rendered, But Hidden When Closed) */}
+
+            {/*             
             <div className={`${styles.content} ${styles.open} overflow-hidden p-2 flex py-2 text-gray-500 z-10`}
                 style={{
                     position: isOpen ? 'relative' : 'absolute',
@@ -34,85 +37,14 @@ export default function Accordion({ title, imageSrc, content, styles, isOpen, on
                     overflow: 'hidden',
                     opacity: isOpen ? 1 : 0,
                     transition: 'opacity 0.3s ease-in-out, height 0.3s ease-in-out'
-                }}
->
+                }} >*/}
+
+            <div 
+                className={`${styles.content} ${styles.open} overflow-hidden p-1 flex py-2 text-gray-500 z-10 
+                transition-opacity duration-300 ease-in-out ${isOpen ? 'relative opacity-100 h-auto' : 'absolute opacity-0 h-0 -left-[9999px]'}`}
+            >
             {content}
             </div> 
         </div>        
     );
 }
-
-
-// export default function Accordion({ title, imageSrc, content, styles, isOpen, onToggle }) {
-//     return (
-//         <div
-//             id={title ? title.toLowerCase().replace(/\s+/g, '_') : ""}
-//             className="px-2 sm:px-6 lg:px-4 relative mt-4"
-//             onClick={() => onToggle(isOpen ? null : title)}
-//         >
-
-//             {/* Closed State */}
-//             <div className={`${styles.closed} ${isOpen ? 'bg-yellow-500' : ''} p-2 cursor-pointer w-full flex items-center`}>
-//                 <div className="z-10 flex items-center font-semibold w-full h-full">
-//                     <div>
-//                         <img src={imageSrc} alt={title} className="w-16 sm:w-32 h-auto rounded-lg" />
-//                     </div>
-//                     <div className="w-full flex justify-center">
-//                         <div className="text-black text-lg sm:text-2xl font-semibold">
-//                             {title}
-//                             <i className={`${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'} fa ml-4`}></i>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-
-//             {/* Open State (Always Rendered, but Hidden When Closed) */}
-//             <div
-//                 className={`${styles.content} ${styles.open} overflow-hidden p-1 flex py-2 text-gray-500 z-10`}
-//                 style={{
-//                     position: isOpen ? 'relative' : 'absolute',
-//                     left: isOpen ? 'auto' : '-9999px',
-//                     height: isOpen ? 'auto' : '0',
-//                     overflow: 'hidden',
-//                     opacity: isOpen ? 1 : 0,
-//                     transition: 'opacity 0.3s ease-in-out'
-//                 }}
-//             >
-//                 {content}
-//             </div>
-//         </div>
-//     );
-// }
-
-
-// export default function Accordion({ title, imageSrc, content, styles, isOpen, onToggle }) {
-//     return (
-//         <div
-//             id={title ? title.toLowerCase().replace(/\s+/g, '_') : ""}
-//             className="px-2 sm:px-6 lg:px-4 relative mt-4"
-//             onClick={() => onToggle(isOpen ? null : title)} // Toggle logic
-//         >
-//             {/* Closed State */}
-//             <div className={`${styles.closed} p-2 cursor-pointer w-full flex items-center`}>
-//                 <div className="z-10 flex items-center font-semibold w-full h-full">
-//                     <div>
-//                         <img src={imageSrc} alt={title} className="w-16 sm:w-32 h-auto rounded-lg" />
-//                     </div>
-//                     <div className="w-full flex justify-center">
-//                         <div className="text-black text-lg sm:text-2xl font-semibold">
-//                             {title}
-//                             <i className={`${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'} fa ml-4`}></i>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-
-//             {/* Open State */}
-//             {isOpen && (
-//                 <div className={`${styles.content} ${styles.open} overflow-hidden p-1 flex py-2 text-gray-500 z-10`}>
-//                     {content}
-//                 </div>
-//             )}
-//         </div>
-//     );
-// }
