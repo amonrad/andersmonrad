2a2913b24f46ff5f0a65a755c41d5f9ce080d4e6
Bo <dysperen@gmail.com>
2025-04-11 13:01:41 +0200

    somethinh

--- src/components/Sections/Artwork/Artwork.js
+++ src/components/Sections/Artwork/Artwork.js
@@ -38,10 +38,12 @@ export default function Artwork() {
 
     return (
         <div className="p-4 text-black bg-white bg-opacity-35 text-center">
-            <p className="mb-4"> "In addition to my musical activities, I’m very interested in visual art and have been doing a lot of visual artwork myself over the years. This is a selection of my acrylic paintings created between 2005-2014.",
-        <br/>,
-        "The style is inspired by 60’s OpArt, but my main interest is a musical, “polyphonic” approach, with multiple, simultaneous motives layered on top of each other. Accordingly, I have been very interested in the direct integration between visuals and sound – which was a key motivation for my work with audio-visual digital projects."
-        </p>
+            <p className="mb-4">
+            In addition to my musical activities, I’m very interested in visual art and have been doing a lot of visual artwork myself over the years. This is a selection of my acrylic paintings created between 2005-2014.
+            <br />
+            The style is inspired by 60’s OpArt, but my main interest is a musical, “polyphonic” approach, with multiple, simultaneous motives layered on top of each other. Accordingly, I have been very interested in the direct integration between visuals and sound – which was a key motivation for my work with audio-visual digital projects.
+            </p>
+
             <div className="grid grid-cols-2 gap-8 mt-8">
                 {shuffledImages.map((src, index) => (
                     <img key={`A-${index}`} src={src} alt={`artwork${index + 1}`} className="w-full h-auto" />
