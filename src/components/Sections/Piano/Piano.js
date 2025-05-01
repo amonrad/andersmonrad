5c0c60ecd00bdf8a7135ccf4f1e9281d86b64c20
Bo <dysperen@gmail.com>
2025-05-01 10:23:07 +0200

    Make sure styling is embedded

--- src/components/Sections/Piano/Piano.js
+++ src/components/Sections/Piano/Piano.js
@@ -1,5 +1,5 @@
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
-//import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 
 export default function Piano() {
     //const pianoText = "Piano-playing is my main artistic background, and has been a central part of my artistic identity since I started playing when I was 6 years old. I played on a high level as a teenager, and participated in a few competitions. In my 20’s I abandoned the piano for a while. In later years, since I started teaching piano, I have taken up my piano playing again, and enjoy playing a wide variety of musical styles - especially the transcendental piano works of the romantic era and beyond. Here’s a little selection.";
@@ -7,23 +7,16 @@ export default function Piano() {
     return (
         <div className="p-4 text-black bg-white bg-opacity-35 text-center">
             <p className="mb-4">Piano-playing is my main artistic background, and has been a central part of my artistic identity since I started playing when I was 6 years old. I played on a high level as a teenager, and participated in a few competitions. In my 20’s I abandoned the piano for a while. In later years, since I started teaching piano, I have taken up my piano playing again, and enjoy playing a wide variety of musical styles - especially the transcendental piano works of the romantic era and beyond. Here’s a little selection.</p>
-
-            <div className="grid grid-cols-3">
-
-                <div
-                className="relative col-span-2 w-full mt-8 ml-auto"
-                style={{ paddingTop: '56.25%' }} // 16:9 aspect ratio
-                >
-                    <div className="absolute top-0 left-0 w-full h-full">
-                        <LiteYouTubeEmbed
-                        id="Ffe-7T6yDhU"
-                        title="UnSospiro Video"
-                        noCookie={true}
-                        poster="maxresdefault"
-                        wrapperClass="w-full h-full"
-                        />
-                    </div>
-                </div>
+            <LiteYouTubeEmbed
+                id="Ffe-7T6yDhU"
+                adNetwork={false}
+                title="Piano play?"
+                poster="maxresdefault"
+                wrapperClass="w-full h-full yt-lite"
+            />
+            
+            {/*<div className="flex items-center justify-center h-96 w-full">
+                
 
                 {/* <div className="relative col-span-2 w-full mt-8 ml-auto" style={{ paddingTop: '56.25%' }} // 16:9 aspect ratio
                 >
@@ -104,9 +97,9 @@ export default function Piano() {
                         title="Bartok Video" 
                         allowFullScreen
                     ></iframe>
-                </div> */}
+                </div> 
 
-            </div>  
+            </div>*/}
         </div>
     );
 }
